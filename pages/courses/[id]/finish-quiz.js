e3d849d4a7ad57681549cc6c4421390ebf20bcea
Sofia Cavelius <sofiacavelius@web.de>
2024-02-27 17:49:24 +0100

    add counter to finishpage

--- pages/courses/[id]/finish-quiz.js
+++ pages/courses/[id]/finish-quiz.js
@@ -1,16 +1,29 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 import Link from "next/link";
 import Logo from "@/components/Logo";
+import { useRouter } from "next/router";
+
+function Counter({ targetCount, className }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count < targetCount) {
+      const interval = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 200); // Intervall in Millisekunden (200ms = 0,2 Sekunden)
+
+      return () => clearInterval(interval);
+    }
+  }, [count, targetCount]);
+
+  return <div className={className}>{count}</div>;
+}
 
 export default function FinishQuizPage() {
-  const router = useRouter();
-  const { isReady } = router;
-  const { id } = router.query;
+  const { id } = useRouter().query;
   const { data: course, isLoading, error } = useSWR(`/api/courses/${id}`);
-
-  const { rightAnswers, wrongAnswers } = router.query;
+  const { rightAnswers, wrongAnswers } = useRouter().query;
   const [parsedRightAnswers, setParsedRightAnswers] = useState([]);
   const [parsedWrongAnswers, setParsedWrongAnswers] = useState([]);
 
@@ -25,15 +38,8 @@ export default function FinishQuizPage() {
 
   if (!course || !course.cards) return null;
 
-  console.log("right Answers:", JSON.parse(rightAnswers));
-  console.log("wrong Answers:", JSON.parse(wrongAnswers));
-  console.log("Course", course);
-
   const wrong = JSON.parse(wrongAnswers);
   const right = JSON.parse(rightAnswers);
-
-  console.log("wrong Answers from finish quiz:", wrong);
-  console.log("right Answers from finish quiz:", right);
 
   // Funktion um die falschen Frage noch einmal zu machen
   const redoQuiz = () => {
@@ -56,11 +62,17 @@ export default function FinishQuizPage() {
             {course.name} Course
           </h1>
           <h2 className="finish-subhead">Right Answers:</h2>
-          <p className="finish-number">{parsedRightAnswers.length}</p>
+          <Counter
+            className="finish-number"
+            targetCount={parsedRightAnswers.length}
+          />
           <h2 className="finish-subhead">Wrong Answers:</h2>
-          <p className="finish-number">{parsedWrongAnswers.length}</p>
+          <Counter
+            className="finish-number"
+            targetCount={parsedWrongAnswers.length}
+          />
         </div>
-        {parsedWrongAnswers.length > 0 && ( // Bedingung für die Anzeige des Buttons
+        {parsedWrongAnswers.length > 0 && (
           <button className="navigation-start-quiz" onClick={redoQuiz}>
             <p className="navigation-text">redo wrong answers</p>
           </button>
